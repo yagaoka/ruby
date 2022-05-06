11107619ace0c87edc52f6e2ad528fab436d493a
Yagashira Yuya <s213346@stn.nagaokaut.ac.jp>
2022-05-06 06:48:18 +0000

    Today's third commit

---
+++ apple.rb
@@ -0,0 +1,9 @@
+apple = "Yamanashi"
+
+if apple == "Aomori"
+  puts "このりんごは青森県産です。"
+elsif apple == "Nagano"
+  puts "このりんごは長野県産です。"
+else
+  puts "このりんごは青森県産でも長野県産でもありません。"
+end
---
+++ hand.rb
@@ -0,0 +1,13 @@
+hand = "グー"
+
+if hand == "グー"
+  puts "出した手はグーです"
+end
+
+if hand != "チョキ"
+  puts "出した手はチョキではありません"
+end
+
+if (hand == "パー") || (hand == "グー")
+  puts "出した手はパーです"
+end
